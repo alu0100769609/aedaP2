e3d84a9af61a7392264a3d44f6ba34f177ac7984
Adexe Sabina <alu0100769609@ull.edu.es>
2016-03-04 18:30:13 +0000

    Cambios en el main y creado .gitignore

--- main/main.cpp
+++ main/main.cpp
@@ -8,22 +8,29 @@ int main(void) {
    bool exitMain = false;
    int option;
 
-   RPN<int>* calculator = new RPN<int>();
    do {
       cout << "Reverse Polish Notation Calculator (RPN Calculator)\n"
            << "---------------------------------------------------\n" << endl;
 
       cout << "    0.- Exit\n"
-           << "    1.- Set & evaluate expression\n" << endl;
+           << "    1.- Set & evaluate int expression\n"
+           << "    2.- Set & evaluate float expression\n" << endl;
       cin >> option;
       system("clear");
       switch (option) {
          case 0:
             exitMain = true;
             break;
-         case 1:
-            calculator->run();
+         case 1: {
+            RPN<int>* intCalculator = new RPN<int>();
+            intCalculator->run();
             break;
+         }
+         case 2: {
+            RPN<float>* floatCalculator = new RPN<float>();
+            floatCalculator->run();
+            break;
+         }
          default:
             cout << "Wrong choice, please try again...\n" << endl;
       }
