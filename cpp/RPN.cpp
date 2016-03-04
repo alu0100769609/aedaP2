f2f2ac641fde8e5885a80009b83108810857e41f
Adexe Sabina <alu0100769609@ull.edu.es>
2016-03-04 00:37:56 +0000

    Creados comentarios en RPN.hpp y creadas etiquetas ToDo en RPN.hpp antes de implementar plantillas

--- cpp/RPN.cpp
+++ cpp/RPN.cpp
@@ -23,13 +23,18 @@ do{
    int c = cin.peek();           // peek character
    if (c == '\n') {
       finCadena = true;
+//ToDo: Llamar a printItems();
       cout << "Resultado: ";
       stack->printItems();
+//FinToDo
    }
    else if (isdigit(c)){
+//ToDo: Llamar a pushItem();
       int n;
       cin >> n;
       stack->setHead(n);
+//FinToDo
+
 //      cout << "Se introdujo en la pila: " << stack->getFirst()->getElement() << endl;
 //      cout << "En la pila están: ";
 //      stack->printItems();
@@ -37,27 +42,30 @@ do{
    else {
       char op;
       cin >> op;
+//ToDo: Llamar a popItems(); que debe llamar a popItem();
       int value1 = stack->getFirst()->getElement();   //get element from stack
       stack->deleteHead();                           //remove element from stack
       int value2 = stack->getFirst()->getElement();   //get element from stack
       stack->deleteHead();                           //remove element from stack
+//FinToDo
       switch (op) {
          case '+':
+//ToDo: Llamar a addition()
             stack->setHead(value2 + value1);
             break;
          case '-':
+//ToDo: Llamar a substract()
             stack->setHead(value2 - value1);
             break;
          case '*':
+//ToDo: Llamar a multiply()
             stack->setHead(value2 * value1);
             break;
          case '/':
+//ToDo: Llamar a divide()
             stack->setHead(value2 / value1);
             break;
       }
-      //   if (c == "+-*/") {//+ = 43; - = 45; * = 42; / = 47;
-//      cout << "You entered the word: " << str << endl;
-//      cout << "No es un número y vale: " << str << endl;
    }
 }while (!finCadena);
 }
--- hpp/RPN.hpp
+++ hpp/RPN.hpp
@@ -1,16 +1,36 @@
 #include "Stack.hpp"
-#include <string>    //string
-#include <cctype>    //isdigit()
+#include <string>             //string
+#include <cctype>             //isdigit()
 
 #include <iostream>
 using namespace std;
 
 class RPN {
-private:
-   //Stack  //almacena valor1 y valor2
+private:                      //Attributes
    Stack stack;
+   int value1;
+   int value2;
+
+public:                       //Default constructor & destructor
+   RPN(void);
+   ~RPN(void);
+
+public:                       //Access methods
+   void setValue1(int);
+   int getValue1(void);
+   void setValue2(int);
+   int getValue2(void);
+   void pushItem(int);        //Push into stack
+   int popItem(void);         //Pop from stack
+
+private:                      //Methods that can't be called from outside the class
+   void addition(void);       //Add two numbers and push result into stack
+   void substract(void);      //Substract two numbers and push result into stack
+   void multiply(void);       //Multiply two numbers and push result into stack
+   void divide(void);         //Divide two numbers and push result into stack
+
 public:
-   RPN();
-   ~RPN();
-   void run();
+   void run(void);            //Run RPN calculator
+   void printItems(void);     //Print stack items
+   void popItems(void);       //Pop 2 items and set them into value1 & value2
 };
